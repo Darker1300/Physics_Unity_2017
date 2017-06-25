e3aeb904eeb2b664426d78559deef53b1d2c97bd
Ethan Moffatt <darker1300@gmail.com>
2017-06-26 02:03:51 +1000

    Added simple controller movement.

---
+++ Physics_Unity_2017/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller = null;
+    public string HorizontalInput = "Horizontal";
+    public string VerticalInput = "Vertical";
+    public string JumpInput = "Jump";
+    public float speed = 10f;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection;
+
+    private void Start()
+    {
+        if (!controller) controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // Calculate moveDirection
+            moveDirection = new Vector3(Input.GetAxis(HorizontalInput), 0, Input.GetAxis(VerticalInput));
+            moveDirection = transform.TransformDirection(moveDirection);
+            // Apply Speed modifier
+            moveDirection *= speed;
+            // Jumping
+            if (Input.GetButton(JumpInput))
+                moveDirection.y = jumpSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!controller) return;
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
+}
